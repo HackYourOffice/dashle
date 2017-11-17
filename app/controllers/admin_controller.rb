aa04daf7b7b69571c55d3d7f293dc275db60c499
Tobias Harms <tobias.harms@dm.de>
2017-11-17 20:06:48 +0100

    Improve the UI

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,19 @@
 class AdminController < ApplicationController
   def show
   end
+
+  def modify
+    if parameter_isset('new_topic_name')
+      # topics.add(params['new_topic_name'])
+      logger.info("New topic: " + params['new_topic_name'])
+    elsif parameter_isset('selection')
+      # show urls section
+      logger.info("Open details for: " + params['selection'])
+      @selected_topic = params['selection']
+    end
+  end
+
+  def parameter_isset(param)
+    defined? param and params[param] != ''
+  end
 end
--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,2 +1,8 @@
 module AdminHelper
+  def all_topics
+    {'' => 0,
+     'Cats' => ['catsource.com', 'getthecat.org', 'reddit.com/r/cats'],
+     'Dashboards' => ['boringkibana.orga/dashboard-1', 'grafana.sucks'],
+     'Presentation' => ['test.de']}
+  end
 end
