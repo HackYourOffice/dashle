41d42c34e5cc876e023296087f2f1df514829ece
Florian Krupicka <krupicka@synyx.de>
2017-11-17 16:50:02 +0100

    Send initial URL on subscriber connect

--- app/channels/url_channel.rb
+++ app/channels/url_channel.rb
@@ -1,9 +1,15 @@
 class UrlChannel < ApplicationCable::Channel
   def subscribed
     stream_from "urls"
+    transmit({:url => determine_initial_url})
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+  end
+
+  private
+  def determine_initial_url
+    'http://placekitten.com/g/400/400'
   end
 end
