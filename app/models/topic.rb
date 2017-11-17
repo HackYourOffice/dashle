0b9627cb7b11732a9ea1e1493792b3bc87a1d264
Florian Krupicka <krupicka@synyx.de>
2017-11-17 22:37:13 +0100

    Scaffold 'topics' controller with Bootstrap Rails support

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,4 +2,12 @@ class Topic < ApplicationRecord
   self.primary_key = 'name'
 
   serialize :urls, Array
+
+  def urls_string
+    urls.join("\n")
+  end
+
+  def urls_string=(url_string)
+    self.urls = url_string.split("\n").map(&:chomp)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  get 'admin/show'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'dashboards#show'
-
   post 'new_url' => 'dashboards#load'
 
   get 'admin' => 'admin#show'
 
   post 'admin' => 'admin#modify'
+
+  resources :topics
 end
