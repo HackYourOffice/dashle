64b0f1ea6bb17df25d5d079a963b9a0abc0ff087
Florian Krupicka <krupicka@synyx.de>
2017-11-18 19:10:01 +0100

    Remove hardcoded deployment config leftovers

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,14 +2,16 @@
 lock "~> 3.10.0"
 
 set :application, "dashle"
-#set :repo_url, "git@github.com:HackYourOffice/dashle.git"
-set :repo_url, "https://github.com/HackYourOffice/dashle.git"
+set :repo_url, `git remote get-url origin`.chomp
 
 # Default branch is :master
 ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
-# Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/var/lib/dashle"
+# Default deploy_to directory is /var/lib/my_app_name
+set :deploy_to, "/var/lib/#{fetch :application}"
+
+set :deploy_user, "dashle"
+set(:deploy_host, 'dashle') unless exists?(:deploy_host)
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,7 +3,8 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "10.0.20.157", user: "dashle", roles: %w{app db web}
+server fetch(:deploy_host), user: fetch(:deploy_user), roles: %w{app db web}
+
 set :rails_env, 'development'
 set :bundle_without, %w{test}.join(' ')
 
