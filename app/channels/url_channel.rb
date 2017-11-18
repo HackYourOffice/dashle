059723b49721399d48a1c0e745bdc3cdcdbed0e7
Ben Antony <antony@synyx.de>
2017-11-18 02:13:33 +0100

    Add Jobs to switch to next url

--- app/channels/url_channel.rb
+++ app/channels/url_channel.rb
@@ -1,6 +1,8 @@
 class UrlChannel < ApplicationCable::Channel
   DEFAULT_URL = 'http://example.com'
   
+  TOPICS = {}
+
   def subscribed
     stream_from "urls_#{params['topic']}"
     transmit({:url => self.class.determine_url(params['topic'])})
@@ -17,15 +19,38 @@ class UrlChannel < ApplicationCable::Channel
     inform_clients(topic)
   end
 
-  private
-  def self.determine_url(topic)
-    url = if topic = Topic.find_by_name(topic)
-            topic.urls.sample
+  def self.get_topic_status_from_db(topic_name, index = 0)
+
+    url = if (topic = Topic.find_by_name(topic_name))
+            if topic.urls.size < index
+              index = 0
+            end
+            topic.urls[index]
           end
-    url || DEFAULT_URL
+
+    thread_status = {
+        :topic_name => topic_name,
+        :url => url || DEFAULT_URL,
+        :index => index,
+        :timer => 5
+    }
+    start_timer(thread_status)
+    TOPICS[topic_name] = thread_status
   end
 
   def self.inform_clients(topic)
     ActionCable.server.broadcast("urls_#{topic}", url: determine_url(topic))
   end
+
+  private
+  def self.determine_url(topic_name)
+    topic_status = TOPICS[topic_name] || get_topic_status_from_db(topic_name)
+    topic_status[:url]
+  end
+
+  def self.start_timer(thread_status)
+    time = thread_status[:timer]
+    TopicSwitcherJob.set(wait: time.second).perform_later(thread_status)
+  end
+
 end
