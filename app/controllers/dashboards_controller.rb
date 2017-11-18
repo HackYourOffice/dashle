58655e2b5b339018d1277cef5f9c1d956db06c2a
Ben Antony <antony@synyx.de>
2017-11-18 04:56:48 +0100

    Remove commented code

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -8,6 +8,5 @@ class DashboardsController < ApplicationController
     logger.info(params)
     
     UrlChannel.set_url_for_topic(params[:topic], params[:url])
-    # ActionCable.server.broadcast("urls_#{params[:topic]}", url: params[:url])
   end
 end
